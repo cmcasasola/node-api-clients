fe1e5d3b6631ce7a09e2db28295ffe96d4668ce8
cmcasasola <cmcasasolah@gmail.com>
2020-04-03 20:30:00 -0500

    Config database url from Mongo cloud

--- config/database.config.js
+++ config/database.config.js
@@ -1,3 +1,4 @@
 module.exports = {
-    url: 'mongodb://localhost:27017/db_clients'
+    // url: 'mongodb://localhost:27017/db_clients'
+    url: 'mongodb+srv://admin:admin123@cluster0-n6uuw.gcp.mongodb.net/test?retryWrites=true&w=majority'
 }
